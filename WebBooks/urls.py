26f3976570a4abc1fdbe2256433a4face3896acb
KonstantinViv <vivivova2047@gmail.com>
2022-10-19 09:28:12 +0300

    006

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from catalog import views
+from django.urls import path
+#from import views
 
+# Listing 9.1
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,23 @@
 # Listing 8.1
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
-
-# Create your views here.
+# Listing 9.4
 def index(request):
-    return HttpResponse("Главная страница сайта  Мир книг!")
-# Create your views here.
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'На складе')
+    # Здесь метод 'all()' применен по умолчанию.
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    # Авторы книг,
+    num_authors = Author.objects.count()
+
+    # Отрисовка HTML-шаблона index.html с данными
+    # внутри переменной context
+    return render(request, 'index.html', context={
+                                         'num_books': num_books,
+                                         'num_instances': num_instances,
+                                         'num_authors': num_authors,
+                                         'num_instances_available': num_instances_available})
